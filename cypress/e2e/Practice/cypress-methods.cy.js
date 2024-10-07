13afc377aa7751a7cbc5ea1f6a195ff5df6641ed
MarikaBBB <mbertelli012@gmail.com>
2024-10-08 00:22:59 +0100

    workshop completed

--- cypress/e2e/Practice/cypress-methods.cy.js
+++ cypress/e2e/Practice/cypress-methods.cy.js
@@ -3,20 +3,29 @@ describe("Important Cypress Methods", () => {
     // Use cy.request() to get all of the posts from the /api/posts endpoint
     // Then use cy.invoke() to 'slice' the response body by 1.
     // Hint: you will need to use cy.wrap() around the response.body before calling .invoke()
+    // 
     // https://docs.cypress.io/api/commands/wrap
+    cy.request("GET", "http://localhost:3000/api/posts").then((response) => {
+      cy.wrap(response.body).invoke("slice", 0, 1);
+    });
   });
 
   it("uses cy.request() and cy.its() to get the first posts ID", () => {
     // Use cy.request() to get all of the posts from the /api/posts endpoint
-    // Then use cy.its() to get the first post and also its id
-    // Write and assertion that the first posts id === 'pre-rendering'
-    // Hint: you will need to use cy.wrap() around the response.body before calling .invoke()
-    // You will also need to use .its() twice.
-    // https://docs.cypress.io/api/commands/wrap
+    cy.request("GET", "http://localhost:3000/api/posts").then((response) => {
+      // Use cy.wrap() to wrap the response.body and then use .its() to get the first post and its id
+      cy.wrap(response.body)
+        .its(0) // Get the first post
+        .its('id') // Get the id of the first post
+        .should('eq', 'pre-rendering'); // Assert that the id is 'pre-rendering'
+    });
   });
 
   it("uses cy.within() to get the <h1> inside of the <header>", () => {
-    // Use cy.within() to get the <h1> nested inside of the <header> element
-    // Then assert that the <h1> contains the correct text.
+    // Use cy.within() to get the <header> element
+    cy.get('header').within(() => {
+      // Then assert that the <h1> contains the correct text.
+      cy.get('h1').should('contain', 'Real World Testing Blog');
+    });
   });
 });
